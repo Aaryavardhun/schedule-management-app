0dffc3b96edfb2ce6914e62f8b8e486f04447f0f
Aaryavardhun <aaryavardhun@gmail.com>
2024-10-16 16:49:45 -0400

    initial Commit

---
+++ src/main/java/com/schedule_management_system/SMApp/SmAppApplication.java
@@ -0,0 +1,13 @@
+package com.schedule_management_system.SMApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/schedule_management_system/SMApp/SmAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.schedule_management_system.SMApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
